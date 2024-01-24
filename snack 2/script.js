bbd726def7b5cc34563706e430bdda4c888bef84
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-24 16:02:15 +0100

    feat: array creato

--- snack 2/script.js
+++ snack 2/script.js
@@ -1,2 +1,37 @@
-let nome = []
-let votoPositivo = [];
+let studenti = [
+    {
+        nome: 'marco della rovere',
+        id: 213,
+        grades: 78
+    },
+    {
+        nome: paola cortellessa,
+        id: 110,
+        grades: 96
+    },
+    {
+        nome: 'andrea mantegna',
+        id: 250,
+        grades: 48
+    },
+    {
+        nome: 'gaia borromini',
+        id: 145,
+        grades: 74
+    },
+    {
+        nome: 'luigi grimaldello',
+        id: 196,
+        grades: 68
+    },
+    {
+        nome: 'piero della francesca',
+        id: 102,
+        grades: 50
+    },
+    {
+        nome: 'francesca da polenta',
+        id: 120,
+        grades: 84
+    }
+];
