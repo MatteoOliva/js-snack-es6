9f5f7ec649444927f05e5d3e1f221cf5c86a7a4c
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-24 14:58:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const vips = [
+    'Dwayne Johnson',
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin',
+  ];
+
