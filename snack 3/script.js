0b13d5e5c1f069b384637e9a2fbf048c26c04374
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-25 12:25:50 +0100

    snack 3

---
+++ snack 3/script.js
@@ -0,0 +1,18 @@
+const bicidacorsa = [
+    {
+    nome: 'cinelli',
+    peso: 6
+},
+{
+    nome: 'colnago',
+    peso: 5
+},
+{
+    nome: 'olmo',
+    peso: 7
+},
+{
+    nome: 'pistidda',
+    peso: 8
+}
+];
