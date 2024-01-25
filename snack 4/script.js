0fcc20f34e2596e7e2527fc62359f259a942a8c2
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-25 16:22:54 +0100

    feat: snack 4 prima parte

--- snack 4/script.js
+++ snack 4/script.js
@@ -36,3 +36,16 @@ const teams = [
     },
   ];
 
+//   - modificare il contenuto delle chiavi dell oggetto : score, foul
+const scorefoulgenerator = teams.map((element) => {
+    //   usare la funzione per i numeri randomici
+    return {
+        name: element.name,
+        score: Math.floor(Math.random() * 10 + 1),
+        foul: Math.floor(Math.random() * 20 + 1)
+    }
+})
+console.log(scorefoulgenerator)
+       
+// - creare un nuovo array di oggetti contententi nomi e falli subiti
+//  - utilizzare il destructuring quindi variabile {} = nome array
